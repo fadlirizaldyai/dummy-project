d883d013f14cf6a8de127a23b931aafb9756c664
fadlirizaldyai <fadly.rizaldy@ai.astra.co.id>
2024-06-21 11:47:32 +0700

    d

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,17 @@ const allowedOrigins = [
 ];
 
 export function middleware(req: NextRequest) {
-  const origin = req.headers.get("origin");
+  const origin = req.headers.get("Origin");
   let response = NextResponse.next();
 
-  response.headers.set("Access-Control-Allow-Origin", "*");
+  // response.headers.set("Access-Control-Allow-Origin", "*");
   // response.headers.set("Access-Control-Allow-Origin", "https://jotter-second.vercel.app");
 
-  // if (origin && allowedOrigins.some((pattern) => pattern.test(origin))) {
-  //   response.headers.set("Access-Control-Allow-Origin", origin);
-  // } else {
-  //   response.headers.set("Access-Control-Allow-Origin", ""); // or you can set it to 'null'
-  // }
+  if (origin && allowedOrigins.some((pattern) => pattern.test(origin))) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  } else {
+    response.headers.set("Access-Control-Allow-Origin", ""); // or you can set it to 'null'
+  }
 
   response.headers.set("Access-Control-Allow-Credentials", "true");
   response.headers.set(
