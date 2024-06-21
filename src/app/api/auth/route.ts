652b678b0e8aafe6afc978d4118d31ec92c1a8a7
fadlirizaldyai <fadly.rizaldy@ai.astra.co.id>
2024-06-21 15:04:48 +0700

    get

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const authorization = request.headers.get("Authorization");
 
   if (!authorization) {
